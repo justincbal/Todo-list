9b6db508a2a605aaec800cc8a36bfc14c776a84e
justincbal <justinseebee@hotmail.com>
2022-12-12 20:47:15 -0500

    add nav bar and add task pop up form

---
+++ src/task.js
@@ -0,0 +1,8 @@
+class Task {
+    constructor(name, description, date, status) {
+        this.taskName = name;
+        this.taskDesc = description;
+        this.taskDate = date;
+        this.taskStatus = status;
+    }
+}
