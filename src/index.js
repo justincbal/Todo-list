f51fde8516a260973329356f944e9d2fd43c9abc
justincbal <justinseebee@hotmail.com>
2022-12-15 22:46:55 -0500

    create general layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import './styles/main.scss';
-import { Project, createProject } from './project';
-import { createTask } from './task';
-const taskBtn = document.querySelector('.taskbtn');
-const addList = document.querySelector('.addList');
-
-// Create a project
- taskBtn.addEventListener('click', createProject);
-
-// Create a task
-addList.addEventListener('click', createTask);
