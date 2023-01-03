c21a9ccef4c01f5821a3c83aa8381aa48bbcf258
justincbal <justinseebee@hotmail.com>
2023-01-03 13:52:35 -0500

    restart functionality

--- src/index.js
+++ src/index.js
@@ -11,16 +11,36 @@ const projectList = new ProjectList();
 // Add a project
 const projectButton = document.querySelector('.project-button');
 
-projectButton.addEventListener('click', () => {
-    projectList.addProj();
-    projectList.updateProjectDisplay();
-});
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
 
-// Add a task
-// const taskButton = document.querySelector('.list-button');
-
-// taskButton.addEventListener('click', () => {
-//     print(projectList.this);
-// })
-
-
+if (storageAvailable('localStorage')) {
+    // Yippee! We can use localStorage awesomeness
+    print('STORAGE CONNECTED');
+  }
+  else {
+    // Too bad, no localStorage for us
+    print('STORAGE UNAVAILABLE');
+  }
